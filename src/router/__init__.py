c8803abae5fde91fa6b573042438672422f381d3
Stanislaw Horna <stanislawhorna@outlook.com>
2025-03-20 00:07:52 +0100

    Feature/authorization (#4)
    * add user status instead of bool columns
    
    * add missing status enum
    
    * add Auth Service
    
    * add endpoint to register user
    
    * move /me to user path

--- src/router/__init__.py
+++ src/router/__init__.py
@@ -1,1 +1,2 @@
 from .user_account import user_router  # noqa: F401
+from .auth import auth_router  # noqa: F401
--- src/router/user_account.py
+++ src/router/user_account.py
@@ -1,13 +1,34 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
-
+from fastapi import APIRouter, Depends, status
+from src.Model.UserAccount import RegisterUser, User
 import src.SQL as SQL
+import src.Service.Auth as Auth
 
 user_router = APIRouter()
 
 
-@user_router.get("/create_user", response_model=list[SQL.Tables.UserAccount])
-async def get_users(
+@user_router.post("/register", status_code=status.HTTP_201_CREATED)
+async def register_user(
     sql_session: Annotated[SQL.AsyncSession, Depends(SQL.get_async_session)],
-) -> list[SQL.Tables.UserAccount]:
-    return (await sql_session.exec(SQL.select(SQL.Tables.UserAccount))).all()
+    user: RegisterUser,
+) -> None:
+    user_account = SQL.Tables.UserAccount(**user.model_dump())
+    sql_session.add(user_account)
+    await sql_session.commit()
+
+
+@user_router.get("/me", response_model=User)
+async def me(
+    user: Annotated[Auth.AuthorizedUser, Depends(Auth.authorized_user())],
+    sql_session: Annotated[SQL.AsyncSession, Depends(SQL.get_async_session)],
+) -> SQL.Tables.UserAccount:
+    """
+    Returns information about logged in user.
+    """
+    return (
+        await sql_session.exec(
+            SQL.select(SQL.Tables.UserAccount).filter(
+                SQL.Tables.UserAccount.id == user.user_id
+            )
+        )
+    ).first()
