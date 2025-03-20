c555680d17ac65954ffc9228616b660e1a0259d8
Stanisław Horna <stanislawhorna@outlook.com>
2025-03-20 22:37:45 +0100

    add CORS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import src.config as config
 import src.SQL.Tables as Tables
@@ -17,6 +18,18 @@ app = FastAPI(
     title=config.APP_TITLE,
     description=config.APP_DESCRIPTION,
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(
     router.user_router,
