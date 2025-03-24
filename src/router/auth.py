a4e3960a3f7a8070a6c533f81acd8de3d00d672b
Stanisław Horna <stanislawhorna@outlook.com>
2025-03-24 20:26:02 +0100

    add endpoint to change password

--- src/router/auth.py
+++ src/router/auth.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 import src.SQL as SQL
 import src.Service.Auth as Auth
 import src.Model.UserAccount as UserModel
@@ -28,3 +28,33 @@ async def logout(
     user: Annotated[Auth.AuthorizedUser, Depends(Auth.authorized_user())],
 ):
     Auth.user_logout(user)
+
+
+@auth_router.post("/change_password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(
+    change_password_request: UserModel.ChangePassword,
+    user: Annotated[Auth.AuthorizedUser, Depends(Auth.authorized_user())],
+    sql_session: Annotated[SQL.AsyncSession, Depends(SQL.async_session_generator)],
+) -> None:
+    if change_password_request.new_password == change_password_request.old_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="New password cannot be the same as old password",
+        )
+
+    DB_user: SQL.Tables.UserAccount = (
+        await sql_session.exec(
+            SQL.select(SQL.Tables.UserAccount).filter(
+                SQL.Tables.UserAccount.id == user.user_id
+            )
+        )
+    ).first()
+
+    if DB_user.password != change_password_request.old_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User is unauthorized"
+        )
+
+    DB_user.password = change_password_request.new_password
+    await sql_session.commit()
+    return None
