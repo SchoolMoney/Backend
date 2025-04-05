8a14f3f67571f1919d80173bbc526109d5c9202e
rukaSUKII <sobonukasz@gmail.com>
2025-04-05 11:49:15 +0200

    Add service for taking care of collecting information for class-view

--- src/Service/ClassView/class_view_operations.py
+++ src/Service/ClassView/class_view_operations.py
@@ -1,12 +1,12 @@
 import asyncio
-from typing import List, Optional, Sequence
+from typing import Optional, Sequence
 
 import src.SQL as SQL
 from src.SQL.Tables import ClassGroup, People, ParentGroupRole
 from src.SQL.Tables.Collection import Collection
 from src.SQL.Enum import CollectionStatus
 
-from src.repository.class_group_repository import get_by_id
+import src.repository.class_group_repository as class_group_repository
 
 
 class_view_response_model = {
@@ -23,7 +23,7 @@ class_view_response_model = {
 }
 
 async def get_children_in_class(session: SQL.AsyncSession, class_id: int) -> Optional[Sequence[People.Child]]:
-    "Get children assigned to a class with given id"
+    """Get children assigned to a class with given id"""
 
     query = SQL.select(
         People.Child).join(
@@ -38,7 +38,7 @@ async def get_children_in_class(session: SQL.AsyncSession, class_id: int) -> Opt
     return children_result
 
 async def get_parents_in_class(session: SQL.AsyncSession, class_id: int) -> Optional[Sequence[dict]]:
-    "Get parents whose children are assigned to a class with given id"
+    """Get parents whose children are assigned to a class with given id"""
 
     query = SQL.select(
         People.Parent.id, People.Parent.name, People.Parent.surname, ParentGroupRole.role, People.Parent.account_id).join(
@@ -65,7 +65,7 @@ async def get_parents_in_class(session: SQL.AsyncSession, class_id: int) -> Opti
     return formatted_parents
 
 async def get_collections_in_class(session: SQL.AsyncSession, class_id: int, status: CollectionStatus) -> Optional[Sequence[Collection]]:
-    "Get collection which belongs to a class with given id and has specified status"
+    """Get collection which belongs to a class with given id and has specified status"""
 
     query = SQL.select(Collection).join(
         ClassGroup, ClassGroup.id == Collection.class_group_id).filter(
@@ -78,20 +78,47 @@ async def get_collections_in_class(session: SQL.AsyncSession, class_id: int, sta
         return None
     return collections_result
 
+async def get_requester_information(session: SQL.AsyncSession, class_id: int, user_id:int) -> Optional[dict] :
+    """Get information about a requester"""
 
-async def collect_class_view_data(session: SQL.AsyncSession, class_id: int, status: CollectionStatus):
+    query = SQL.select(
+        People.Parent.id,
+        People.Parent.name,
+        People.Parent.surname,
+        ParentGroupRole.role
+    ).join(
+        ParentGroupRole, People.Parent.id == ParentGroupRole.parent_id
+    ).join(
+        People.UserAccount, People.Parent.account_id == People.UserAccount.id
+    ).filter(ParentGroupRole.class_group_id == class_id, People.UserAccount.id == user_id)
+
+    requester_data = await session.exec(query)
+    requester_record = requester_data.first()
+    if requester_record is None:
+        return None
+    return {
+        'parent_id': requester_record[0],
+        'name': requester_record[1],
+        'surname': requester_record[2],
+        'group_role': requester_record[3],
+    }
+
+
+async def collect_class_view_data(class_id: int, status: CollectionStatus, user_id: int) -> dict:
     """Collect data for class view by running all queries concurrently"""
 
-    children, parents, collections, class_group = await asyncio.gather(
-        get_children_in_class(session, class_id),
-        get_parents_in_class(session, class_id),
-        get_collections_in_class(session, class_id, status),
-        get_by_id(session, class_id)
+    children, parents, collections, class_group, requester = await asyncio.gather(
+        get_children_in_class(await SQL.get_async_session(), class_id),
+        get_parents_in_class(await SQL.get_async_session(), class_id),
+        get_collections_in_class(await SQL.get_async_session(), class_id, status),
+        class_group_repository.get_by_id(await SQL.get_async_session(), class_id),
+        get_requester_information(await SQL.get_async_session(), class_id, user_id)
     )
 
     return {
         "class": class_group.model_dump() if class_group else None,
         "children": [child.model_dump() for child in children],
-        "parents": parents,  # Już są w formacie słownika z nazwami pól
+        "parents": parents,
         "collections": [collection.model_dump() for collection in collections],
+        'requester': requester
     }
