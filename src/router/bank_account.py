fbcf17303476b03e8c1158be759ae102ae2b2ee4
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-04-09 01:09:12 +0200

    fix getting single bank account

--- src/router/bank_account.py
+++ src/router/bank_account.py
@@ -9,12 +9,12 @@ from src.repository import bank_account_repository
 bank_account_router = APIRouter()
 
 
-@bank_account_router.get("/{bank_account_id}", status_code=status.HTTP_200_OK, response_model=List[BankAccount])
+@bank_account_router.get("/{bank_account_id}", status_code=status.HTTP_200_OK, response_model=BankAccount)
 async def get(
     user: Annotated[Auth.AuthorizedUser, Depends(Auth.authorized_user())],
     bank_account_id: int,
     sql_session: Annotated[SQL.AsyncSession, Depends(SQL.get_async_session)],
-) -> Sequence[BankAccount]:
+) -> BankAccount:
     try:
         return await bank_account_repository.get_by_id(sql_session, bank_account_id)
     except HTTPException:
