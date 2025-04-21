46192cfc3dc78368a333948814de6c4140087a85
rukaSUKII <sobonukasz@gmail.com>
2025-04-21 09:13:38 +0200

    Adjust models in reference to database table

--- src/Model/CollectionDocument.py
+++ src/Model/CollectionDocument.py
@@ -1,12 +1,27 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class CollectionDocument(BaseModel):
-    document_id: int | None = None
+    document_id: Optional[int] = None
     collection_id: int
     document_name: str
-    document_path: str
+    file_type: str
+    file_data: bytes
 
 class CreateCollectionDocument(BaseModel):
     collection_id: int
     document_name: str
-    document_path: str
+    file_type: str
+    file_data: str
+
+class CreateCollectionDocumentDB(BaseModel):
+    collection_id: int
+    document_name: str
+    file_type: str
+    file_data: bytes
+
+class CollectionDocumentMetadata(BaseModel):
+    document_id: Optional[int] = None
+    collection_id: int
+    document_name: str
+    file_type: str
